54f4f76e3e80127073246cc6308bbc9e0359ce7b
Eduardo Perez <edujpp1@gmail.com>
2019-07-23 08:24:35 -0300

    Versión final. Se muestra la información de la contización

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -7,6 +7,11 @@ const Cotizacion = ({resultado}) => {
   return(
     <div className="resultado">
       <h2>Resultado</h2>
+      <p className="precio">Precio actual: <span>{resultado.PRICE}</span></p>
+      <p>Precio más alto del día: <span>{resultado.HIGHDAY}</span></p>
+      <p>Precio más bajo del día: <span>{resultado.LOWDAY}</span></p>
+      <p>Variación del precio en las últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR} %</span></p>
+      <p>Última actualización: <span>{resultado.LASTUPDATE}</span></p>
     </div>
   );
 }
