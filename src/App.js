4ae87ba3190793eaffd554b69139ca5d12d1e17a
Eduardo Perez <edujpp1@gmail.com>
2019-07-23 08:05:02 -0300

    Mostrando un Spinner de carga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Formulario from './components/Formulario';
+import Spinner from './components/Spinner';
 import imagen from './cryptomonedas.png'
 
 function App() {
 
   const[moneda, guardarMoneda] = useState('');
   const[criptomoneda, guardarCriptomoneda] = useState('');
+  const[cargando, guardarCargando] = useState(false);
 
   useEffect(() => {
     const cotizarCriptomoneda = async () => {
+
+      // Si no hay moneda, no ejecutar
+      if(moneda==='') return;
+      
       const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
       const resultado = await axios.get(url);
 
-      console.log(resultado)
+      console.log(resultado);
+
+      guardarCargando(true);
+      setTimeout(() => {
+        guardarCargando(false);
+      }, 3000);
     }
 
     cotizarCriptomoneda();
   }, [criptomoneda, moneda]);
+
+  // Mostrar Spinner o resultado
+  const componente = (cargando) ? <Spinner /> : null;
 
   return (
     <div className="container">
@@ -31,6 +45,7 @@ function App() {
             guardarMoneda={guardarMoneda}
             guardarCriptomoneda={guardarCriptomoneda}
           />
+          {componente}
         </div>
       </div>
     </div>
